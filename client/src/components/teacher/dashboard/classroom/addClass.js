f4553b7fc5aaa4977a12f8fe85a4311dd9823d1f
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-10-18 20:16:30 +0530

    added addClassDropDown component

--- client/src/components/teacher/dashboard/classroom/addClass.js
+++ client/src/components/teacher/dashboard/classroom/addClass.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardActions from '@material-ui/core/CardActions'
@@ -6,28 +6,33 @@ import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
 import Add from '@material-ui/icons/Add'
-
+import AddClassDropDown from './addClassDropdown'
 const AddClass = props => {
+  const [openAddClassModal, setOpenAddClassModal] = useState(false);
+  const handleToggle = () => {
+    setOpenAddClassModal(!openAddClassModal)
+  };
   return (
     <Grid item>
       <Card style={{ maxWidth: 350, minWidth: 300 }}>
-        <CardActionArea>
+        <CardActionArea onClick={handleToggle}>
           <AddCircleIcon
             color='primary'
             style={{
               fontSize: 'large',
               width: 90,
               height: 90,
-              marginLeft: '40%'
+              marginLeft: '35%'
             }}
           />
         </CardActionArea>
         <CardActions>
-          <Button size='small' color='primary' startIcon={<Add />}>
+          <Button onClick={handleToggle} size='small' color='primary' startIcon={<Add />}>
             Add new classroom
           </Button>
         </CardActions>
       </Card>
+      <AddClassDropDown handleToggle={handleToggle} openAddClassModal={openAddClassModal} />
     </Grid>
   )
 }
--- client/src/components/teacher/dashboard/classroom/addClassDropdown.js
+++ client/src/components/teacher/dashboard/classroom/addClassDropdown.js
@@ -2,10 +2,7 @@ import React, { Fragment, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import { Button, Menu,  MenuItem, TextField } from '@material-ui/core';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import AssignmentTurnedInSharpIcon from '@material-ui/icons/AssignmentTurnedInSharp';
 import { useDispatch, useSelector } from 'react-redux';
-import { AddReply } from '../../redux/actions/forum';
 
 const useStyles = makeStyles((theme) =>( {
   list: {
@@ -17,67 +14,63 @@ const useStyles = makeStyles((theme) =>( {
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: '15ch',
     },
   },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    width: '100ch',
+    width: '50ch',
   },
 }));
 
-const Reply = ({showReplyModal, handleReplyClick, id, type}) => {
+const AddClassDropDown = ({handleToggle, openAddClassModal}) => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const [reply, setReply] = useState('');
-  const [anchorEl, setAnchorEl] = useState(null);
-  
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const [subject, setSubject] = useState('');
+  const [code, setCode] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(AddReply(type, reply, id));
-    handleClose();
-    handleReplyClick()
+    handleToggle();
   }
 
   return (
     <Fragment>
-      <Drawer open={showReplyModal} onClose={handleReplyClick}  anchor='bottom'>
+      <Drawer open={openAddClassModal} onClose={handleToggle}  anchor='bottom'>
         <div style={{marginLeft: 20}}>
-          <h2>Reply</h2>
+          <h2>Enter Subject Details</h2>
         </div>
         <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="on">
           <div>
             <TextField
               id="outlined-multiline-static"
-              label="Type Reply here...."
-              multiline
-              rows={4}
+              label="Subject Name"
               className={classes.textField}
               variant="outlined"
+              value={subject}
               required
-              value={reply}
-              onChange={(e) => setReply(e.target.value)}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+            <TextField
+              id="outlined-multiline-static"
+              label="Subject Code"
+              className={classes.textField}
+              variant="outlined"
+              value={code}
+              style={{marginTop: 15}}
+              required
+              onChange={(e) => setCode(e.target.value)}
             />
           </div>
           <div style={{margin: 20, display: 'flex'}}>
             <div>
               <Button variant="contained" color="primary" type="submit" >
-                <AssignmentTurnedInSharpIcon />
                 Submit
               </Button>
             </div>
             <div style={{marginLeft: 20}}>
-              <Button variant="contained" color="secondary" onClick={handleReplyClick}>
-                <AssignmentTurnedInSharpIcon />
+              <Button variant="contained" color="secondary" onClick={handleToggle}>
                 Cancel
               </Button>
             </div>
@@ -88,4 +81,4 @@ const Reply = ({showReplyModal, handleReplyClick, id, type}) => {
   );
 }
  
-export default Reply;
+export default AddClassDropDown;
